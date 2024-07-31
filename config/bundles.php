f1546fb3963ca6183ef096f42ae654e45223b658
v.bissay <v.bissay@cicd.biz>
2024-07-31 15:05:58 +0200

    Ajout bundle knpPaginator

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
