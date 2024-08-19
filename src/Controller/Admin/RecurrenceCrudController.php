d318c19ee8efe254c70329fb0b7e0f423afbd2f8
v.bissay <v.bissay@cicd.biz>
2024-08-19 15:47:08 +0200

    📦 NEW:  ajout authentification partie administration

--- src/Controller/Admin/RecurrenceCrudController.php
+++ src/Controller/Admin/RecurrenceCrudController.php
@@ -4,8 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Recurrence;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class RecurrenceCrudController extends AbstractCrudController
@@ -15,14 +15,15 @@ class RecurrenceCrudController extends AbstractCrudController
         return Recurrence::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            DateField::new('startDate'),
+            DateField::new('endDate'),
+            DateField::new('createdAt')->hideOnForm(),
+            DateField::new('updatedAt')->hideOnForm(),
         ];
     }
-    */
 }
