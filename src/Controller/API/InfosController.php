0edad048539a3a024579f30a2809f701f9f57b81
v.bissay <v.bissay@cicd.biz>
2025-07-02 12:52:12 +0200

    fix erreur

--- src/Controller/API/InfosController.php
+++ src/Controller/API/InfosController.php
@@ -4,10 +4,11 @@ namespace App\Controller\API;
 
 use Doctrine\DBAL\Connection;
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class InfosController
+class InfosController extends AbstractController
 {
     public function __construct(
         private Connection $connection,
