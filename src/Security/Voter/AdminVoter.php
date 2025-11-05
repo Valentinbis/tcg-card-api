51aa29fee94a5c2d5d45325e1727bf789c794128
v.bissay <v.bissay@cicd.biz>
2025-11-05 10:36:47 +0100

    fix deprecated

--- src/Security/Voter/AdminVoter.php
+++ src/Security/Voter/AdminVoter.php
@@ -4,6 +4,7 @@ namespace App\Security\Voter;
 
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class AdminVoter extends Voter
@@ -14,7 +15,7 @@ class AdminVoter extends Voter
         return true;
     }
 
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
         $user = $token->getUser();
 
--- src/Security/Voter/UserVoter.php
+++ src/Security/Voter/UserVoter.php
@@ -4,6 +4,7 @@ namespace App\Security\Voter;
 
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserVoter extends Voter
@@ -19,7 +20,7 @@ class UserVoter extends Voter
             && $subject instanceof \App\Entity\User;
     }
 
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
         $user = $token->getUser();
 
